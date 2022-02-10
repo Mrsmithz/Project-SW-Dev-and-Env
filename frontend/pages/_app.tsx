6f80bfbf1b38d60350c604fd6627135c6c34ea0c
TeerapatEarth <earth_scorpion@hotmail.com>
2022-02-10 16:28:37 +0700

    [Update] Update font

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,12 +1,19 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
-import Navbar from "../components/Navbar"
+import Navbar from "../components/Navbar";
+import { extendTheme } from "@chakra-ui/react";
+import "@fontsource/prompt/400.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const theme = extendTheme({
+    fonts: {
+      body: "Prompt",
+    },
+  });
   return (
-    <ChakraProvider>
-      <Navbar/>
+    <ChakraProvider theme={theme}>
+      <Navbar />
       <Component {...pageProps} />
     </ChakraProvider>
   );
