a673f4d0a40f83b58d0721d33553acc6318a7819
TeerapatEarth <earth_scorpion@hotmail.com>
2022-02-10 14:35:13 +0700

    [Create] Create navbar template, notification list and user button list

---
+++ frontend/components/Navbar.tsx
@@ -0,0 +1,108 @@
+import { ReactNode } from "react";
+import {
+  Box,
+  Flex,
+  Avatar,
+  Link,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Center,
+  Portal,
+  HStack,
+} from "@chakra-ui/react";
+import { MoonIcon, SunIcon, BellIcon } from "@chakra-ui/icons";
+
+const NavLink = ({ children }: { children: ReactNode }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={"md"}
+    _hover={{
+      textDecoration: "none",
+      bg: useColorModeValue("gray.200", "gray.700"),
+    }}
+    href={"#"}
+  >
+    {children}
+  </Link>
+);
+
+export default function Nav() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <HStack spacing={20}>
+            <Box>Dockie</Box>
+            <Box>Home</Box>
+            <Box>Forum</Box>
+            <Box>Ranking</Box>
+          </HStack>
+          <Flex alignItems={"center"}>
+            <Stack direction={"row"} spacing={7}>
+              <Button onClick={toggleColorMode}>
+                {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+              </Button>
+              <Menu>
+                <MenuButton>
+                  <BellIcon w={5} h={10} />
+                </MenuButton>
+                <Portal>
+                  <MenuList>
+                    <MenuItem>Noti 1</MenuItem>
+                    <MenuItem>Noti 2</MenuItem>
+                    <MenuItem>Noti 3</MenuItem>
+                    <MenuItem>Noti 4</MenuItem>
+                    <MenuItem>Noti 5</MenuItem>
+                  </MenuList>
+                </Portal>
+              </Menu>
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  rounded={"full"}
+                  variant={"link"}
+                  cursor={"pointer"}
+                  minW={0}
+                >
+                  <Avatar
+                    size={"sm"}
+                    src={"https://avatars.dicebear.com/api/male/username.svg"}
+                  />
+                </MenuButton>
+                <MenuList alignItems={"center"}>
+                  <br />
+                  <Center>
+                    <Avatar
+                      size={"2xl"}
+                      src={"https://avatars.dicebear.com/api/male/username.svg"}
+                    />
+                  </Center>
+                  <br />
+                  <Center>
+                    <p>Username</p>
+                  </Center>
+                  <br />
+                  <MenuDivider />
+                  <MenuItem>Your Servers</MenuItem>
+                  <MenuItem>Account Settings</MenuItem>
+                  <MenuItem>Logout</MenuItem>
+                </MenuList>
+              </Menu>
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  );
+}
