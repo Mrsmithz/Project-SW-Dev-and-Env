07a7e8e846674b19953ab1a176dca884ff565852
TeerapatEarth <earth_scorpion@hotmail.com>
2022-02-10 19:05:56 +0700

    [Update] Update responsive navbar

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,9 +1,11 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import {
   Box,
   Flex,
   Avatar,
+  HStack,
   Link,
+  IconButton,
   Button,
   Menu,
   MenuButton,
@@ -13,12 +15,22 @@ import {
   useDisclosure,
   useColorModeValue,
   Stack,
-  useColorMode,
   Center,
+  useColorMode,
   Portal,
-  HStack,
+  Text,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
 } from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 import { MoonIcon, SunIcon, BellIcon } from "@chakra-ui/icons";
+
+const Links = ["Home", "Forum", "Ranking"];
 
 const NavLink = ({ children }: { children: ReactNode }) => (
   <Link
@@ -35,26 +47,62 @@ const NavLink = ({ children }: { children: ReactNode }) => (
   </Link>
 );
 
-export default function Nav() {
+export default function Simple() {
   const { colorMode, toggleColorMode } = useColorMode();
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [openModal, setOpenModal] = useState(false);
+
   return (
     <>
       <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
         <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
-          <HStack spacing={20}>
-            <Box>Dockie</Box>
-            <Box>Home</Box>
-            <Box>Forum</Box>
-            <Box>Ranking</Box>
+          <IconButton
+            size={"md"}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={"Open Menu"}
+            display={{ md: "none" }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={10} alignItems={"center"}>
+            <Box ml={5}>
+              <Text fontSize="3xl">Dockie</Text>
+            </Box>
+            <HStack
+              as={"nav"}
+              spacing={4}
+              display={{ base: "none", md: "flex" }}
+            >
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </HStack>
           </HStack>
           <Flex alignItems={"center"}>
-            <Stack direction={"row"} spacing={7}>
-              <Button onClick={toggleColorMode}>
-                {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
-              </Button>
+            <Stack
+              spacing={2}
+              direction={"row"}
+              display={{ base: "none", md: "flex" }}
+            >
               <Menu>
-                <MenuButton>
+                <MenuButton
+                  onClick={toggleColorMode}
+                  _hover={{ bg: useColorModeValue("yellow.300", "orange.500") }}
+                  w={10}
+                  borderRadius={100}
+                >
+                  {colorMode === "light" ? (
+                    <MoonIcon w={5} h={10} />
+                  ) : (
+                    <SunIcon w={5} h={10} />
+                  )}
+                </MenuButton>
+              </Menu>
+              <Menu>
+                <MenuButton
+                  _hover={{ bg: useColorModeValue("blue.100", "blue.500") }}
+                  w={10}
+                  borderRadius={100}
+                >
                   <BellIcon w={5} h={10} />
                 </MenuButton>
                 <Portal>
@@ -67,6 +115,8 @@ export default function Nav() {
                   </MenuList>
                 </Portal>
               </Menu>
+            </Stack>
+            <Box ml={5}>
               <Menu>
                 <MenuButton
                   as={Button}
@@ -99,9 +149,52 @@ export default function Nav() {
                   <MenuItem>Logout</MenuItem>
                 </MenuList>
               </Menu>
-            </Stack>
+            </Box>
           </Flex>
         </Flex>
+
+        {isOpen ? (
+          <Box pb={4} display={{ md: "none" }}>
+            <Stack as={"nav"} spacing={4} alignItems="center">
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+              <Box onClick={toggleColorMode}>
+                {colorMode === "light" ? (
+                  <Text>Dark Mode</Text>
+                ) : (
+                  <Text>Light Mode</Text>
+                )}
+              </Box>
+              <Box onClick={() => setOpenModal(true)}>Notification</Box>
+            </Stack>
+          </Box>
+        ) : null}
+
+        <Modal isOpen={openModal} onClose={() => setOpenModal(false)} isCentered>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Modal Title</ModalHeader>
+            <ModalCloseButton onClick={() => setOpenModal(false)} />
+            <ModalBody>
+                <Box>Noti 1</Box>
+                <Box>Noti 2</Box>
+                <Box>Noti 3</Box>
+                <Box>Noti 4</Box>
+                <Box>Noti 5</Box>
+            </ModalBody>
+
+            <ModalFooter>
+              <Button
+                colorScheme="blue"
+                mr={3}
+                onClick={() => setOpenModal(false)}
+              >
+                Close
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
       </Box>
     </>
   );
