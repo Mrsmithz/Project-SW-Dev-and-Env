76111cec23e2100bbce0bec081f7ee2a8aab00e6
Mrsmithz <Dechapon.n@hotmail.com>
2022-02-14 02:05:52 +0700

    [Update] Add simple API key checking for backend

--- backend/App.ts
+++ backend/App.ts
@@ -13,7 +13,12 @@ app.use(morgan('dev'))
 app.use(cors())
 app.use(express.json())
 
-
+app.all('*', (req : Request, res : Response, next : NextFunction) : Response => {
+    if (req.headers.authorization !== process.env.API_KEY){
+        return res.sendStatus(401)
+    }
+    next()
+})
 app.get('/api/health', (req : Request, res : Response, next : NextFunction) : Response => {
     return res.json({
         status:'UP'
