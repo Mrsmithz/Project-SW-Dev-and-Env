67eb3a708b6034c93cc5a296e3f356f75baded5a
TeerapatEarth <earth_scorpion@hotmail.com>
2022-02-26 17:24:13 +0700

    [Update] Update test fab

--- frontend/src/test/utils/CreatePost.test.tsx
+++ frontend/src/test/utils/CreatePost.test.tsx
@@ -1,59 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import CreatePost from "../../pages/createpost/index";
 import Fab from "../../components/createpost/Fab";
-import CreatePostForm from "../../components/createpost/CreatePostForm";
-import UploadFile from "../../components/createpost/UploadFile";
-import TaskProgress from "../../components/createpost/TaskProgress";
 import "@testing-library/jest-dom";
-import { Icon } from "@chakra-ui/react";
-import { MdDescription, MdEdit, MdVerified } from "react-icons/md";
-
-const tasks = [
-  {
-    taskName: "Upload File",
-    icon: <Icon as={MdDescription} />,
-  },
-  {
-    taskName: "Fill Information",
-    icon: <Icon as={MdEdit} />,
-  },
-  {
-    taskName: "Confirm",
-    icon: <Icon as={MdVerified} />,
-  },
-];
 
 describe("Create Post Page", () => {
   it("render create post page", () => {
     render(<CreatePost />);
-    const textTitle = screen.getByText(/สร้างโพสต์ใหม่/i);
+    const textTitle = screen.getByText(/Create Post/i);
     expect(textTitle).toBeInTheDocument();
   });
 
   it("render fab", () => {
-    render(<Fab />);
+    const fab = render(<Fab/>)
+    expect(fab.getByRole("button")).toHaveAttribute("href", "/createpost")
   });
 
-  it("render create post form", () => {
-    render(<CreatePostForm toNextPage={() => {}} backPage={() => {}} />);
-  });
-  it("render preview post", () => {
-    //
-  });
-
-  it("render task progress state 1", () => {
-    render(<TaskProgress tasks={tasks} state={1} />);
-  });
-
-  it("render task progress state 2", () => {
-    render(<TaskProgress tasks={tasks} state={2} />);
-  });
-
-  it("render task progress state 3", () => {
-    render(<TaskProgress tasks={tasks} state={3} />);
-  });
-
-  it("render upload file", () => {
-    render(<UploadFile toNextPage={() => {}} file={null} setFile={() => {}} />);
-  });
 });
