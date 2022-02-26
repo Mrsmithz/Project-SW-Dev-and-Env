4acbeedb12f12af32e2934fb410ae27b50ae8ce7
TeerapatEarth <earth_scorpion@hotmail.com>
2022-02-26 16:10:46 +0700

    [Create] Create component test

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,4 +2,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transformIgnorePatterns: ['node_modules/(?!(sucrase)/)'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx|mjs|scss|css)$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    "^.+\\.(css|less|scss)$": "identity-obj-proxy"
+  },
+  testEnvironment: "jsdom"
 };
